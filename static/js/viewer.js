c5ce04c5673e596c3395c24815bb3fd9616b2005
Zade <sirc3h4o2@gmail.com>
2021-02-17 21:44:41 +0100

    added test Plotly table

--- static/js/viewer.js
+++ static/js/viewer.js
@@ -19,7 +19,6 @@ function openOptions(evt, tabName) {
 
 }
 
-
 $(function() {
   document.getElementById("defaultopen").click();
   var lookup = {
@@ -123,7 +122,7 @@ $(function() {
     });
   }
 
-  var layout = {
+  var graph_layout = {
     // xaxis: {
     //   range: ['2020-10-01', '2020-12-09']
     // },
@@ -150,9 +149,37 @@ $(function() {
     }]
   };
 
+
+	var overview_values = [
+	    ['Salaries', 'Office', 'Merchandise', 'Legal', '<b>TOTAL</b>'],
+      [1200000, 20000, 80000, 2000, 12120000],
+      [1300000, 20000, 70000, 2000, 130902000],
+      [1300000, 20000, 120000, 2000, 131222000],
+      [1400000, 20000, 90000, 2000, 14102000]]
+
+	
+	var overview_data = [{
+  	type: 'table',
+  	header: {
+    	values: [["Date"], ["GB1"],["GB2"], ["GB3"], ["GB4"], ["GB5"]],
+    	align: "center",
+    	line: {width: 1, color: 'black'},
+    	fill: {color: "grey"},
+    	font: {family: "Arial", size: 12, color: "white"}
+  	},
+  	cells: {
+    	values: overview_values,
+    	align: "center",
+    	line: {color: "black", width: 1},
+    	font: {family: "Arial", size: 11, color: ["black"]}
+  	}
+	}]
+
+
   Plotly.newPlot('graph', {
     data: traces,
-    layout: layout,
+    layout: graph_layout,
     frames: frames
   });
+	Plotly.newPlot('overview', overview_data);
 });
